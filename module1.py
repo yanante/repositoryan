583e8af4bb108b9eb8f16e20060f866fdbe91a5c
yan <saydashev.yan@mail.ru>
2021-01-28 17:34:05 +0600

    commit message

--- module1.py
+++ module1.py
@@ -1,11 +1,13 @@
 with open('Perepis.txt','r') as f:
     k=0
+    a=int(input())
+    b=int(input())
     for i in f:
         s=i[-5:]
         y=int(s)
         l=i.split()
-        if y<1978:
-            print(l[0])
+        if (a<=y<=b) or (a>=y>=b):
+            print(l[0],l[1],l[2],l[3])
             k+=1
-
-print(k)
+print()
+print('Кол-во вилиджеров:',k)
