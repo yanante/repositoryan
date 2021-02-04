8a3dd57ee4bde461b7a4cfbccf08784c7f43d3d1
yan <saydashev.yan@mail.ru>
2021-02-04 17:35:11 +0600

    commit message

--- module3.py
+++ module3.py
@@ -1,19 +1,27 @@
-def speed():
-    a=(v1-v2)/t
-    print(a)
+s=0
+a=0
 
 def dcf(speed):
-    def rast():
-        def speed():
-          s=(v1*t)+(a*t*t)
+    def rast(v1,v2,t,a):
+        speed(v1,v2,t,a)
+        s=(v1*t)+((a*t*t)/2)
+        print('раст.',s)
     return rast
+
+def speed(v1,v2,t,a):
+    a=(v1-v2)/t
+    print('ускор.',a)
 
 try:
     v1=float(input('нач скорость'))
     v2=float(input('кон скорость'))
     t=float(input('время'))
+    speed=dcf(speed)
+    speed(v1,v2,t,a)
 
-except TypeError:
+except ValueError:
     print('числа писать надо, брат')
 except ZeroDivisionError:
     print('время не то, брат')
+
+
